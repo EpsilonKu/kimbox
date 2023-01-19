5dd92a4a8c5f3d8ce916b448da207ddb412399d8
Kurenshe Nurdaulet <63652620+EpsilonKu@users.noreply.github.com>
2023-01-19 21:32:31 +0600

    Basic pallettes

--- lua/kimbox/init.lua
+++ lua/kimbox/init.lua
@@ -7,133 +7,133 @@ local g = vim.g
 local fn = vim.fn
 local cmd = vim.cmd
 
-M.bgs_list = {"medium", "ocean", "vscode", "deep", "darker"}
+M.bgs_list = { "medium" }
 
 local default_config = {
-    -- Main options --
-    style = "ocean", -- choose between "medium", "ocean", "vscode", "deep", "darker"
-    toggle_style_key = "<Leader>ts",
-    toggle_style_list = M.bgs_list,
-    -- New Lua-Treesitter highlight groups
-    -- This isn't bulletproof, but it will help
-    langs08 = utils.tern(utils.has08(), true, false),
-    -- Used with popup menus (coc.nvim mainly) --
-    popup = {
-        background = false -- use background color for pmenu
-    },
-    -- Plugins Related --
-    diagnostics = {
-        -- TODO: Check this for diagnostics specifically
-        background = true -- use background color for virtual text
-    },
-    -- General formatting --
-    allow_bold = true,
-    allow_italic = false,
-    allow_underline = false,
-    allow_undercurl = true,
-    allow_reverse = false,
-    transparent = false, -- don't set background
-    term_colors = true, -- if true enable the terminal
-    ending_tildes = false, -- show the end-of-buffer tildes
-    -- Custom Highlights --
-    colors = {}, -- Override default colors
-    highlights = {}, -- Override highlight groups
-    -- Plugins or langauges that can be disabled
-    disabled = {
-        langs = {},
-        langs08 = {},
-        plugins = {}
-    },
-    run_before = nil, -- Run a function before the colorscheme is loaded
-    run_after = nil -- Run a function after the colorscheme is loaded
+  -- Main options --
+  style = "medium", -- choose between "medium", "ocean", "vscode", "deep", "darker"
+  toggle_style_key = "<Leader>ts",
+  toggle_style_list = M.bgs_list,
+  -- New Lua-Treesitter highlight groups
+  -- This isn't bulletproof, but it will help
+  langs08 = utils.tern(utils.has08(), true, false),
+  -- Used with popup menus (coc.nvim mainly) --
+  popup = {
+    background = false -- use background color for pmenu
+  },
+  -- Plugins Related --
+  diagnostics = {
+    -- TODO: Check this for diagnostics specifically
+    background = true -- use background color for virtual text
+  },
+  -- General formatting --
+  allow_bold = true,
+  allow_italic = false,
+  allow_underline = false,
+  allow_undercurl = true,
+  allow_reverse = false,
+  transparent = false, -- don't set background
+  term_colors = true, -- if true enable the terminal
+  ending_tildes = false, -- show the end-of-buffer tildes
+  -- Custom Highlights --
+  colors = {}, -- Override default colors
+  highlights = {}, -- Override highlight groups
+  -- Plugins or langauges that can be disabled
+  disabled = {
+    langs = {},
+    langs08 = {},
+    plugins = {}
+  },
+  run_before = nil, -- Run a function before the colorscheme is loaded
+  run_after = nil -- Run a function after the colorscheme is loaded
 }
 
 ---Change kimbox option (g.kimbox_config.option)
 ---@param opt string: option name
 ---@param value any: new value
 function M.set_options(opt, value)
-    local cfg = g.kimbox_config
-    cfg[opt] = value
-    g.kimbox_config = cfg
+  local cfg = g.kimbox_config
+  cfg[opt] = value
+  g.kimbox_config = cfg
 end
 
 ---Apply the colorscheme (same as ':colorscheme kimbox')
 function M.colorscheme()
-    if g.kimbox_config.run_before ~= nil and type(g.kimbox_config.run_before) == "function" then
-        if not pcall(g.kimbox_config.run_before) then
-            log.err("failed running 'before' function", true)
-        end
+  if g.kimbox_config.run_before ~= nil and type(g.kimbox_config.run_before) == "function" then
+    if not pcall(g.kimbox_config.run_before) then
+      log.err("failed running 'before' function", true)
     end
+  end
 
-    cmd("hi clear")
-    if fn.exists("syntax_on") then
-        cmd("syntax reset")
+  cmd("hi clear")
+  if fn.exists("syntax_on") then
+    cmd("syntax reset")
+  end
+
+  vim.o.termguicolors = true
+  g.colors_name = "kimbox"
+
+  M.set_options("style", g.kimbox_config.style)
+
+  require("kimbox.highlights").setup()
+  require("kimbox.terminal").setup()
+
+  if g.kimbox_config.run_after ~= nil and type(g.kimbox_config.run_after) == "function" then
+    if not pcall(g.kimbox_config.run_after) then
+      log.err("failed running 'after' function", true)
     end
-
-    vim.o.termguicolors = true
-    g.colors_name = "kimbox"
-
-    M.set_options("style", g.kimbox_config.style)
-
-    require("kimbox.highlights").setup()
-    require("kimbox.terminal").setup()
-
-    if g.kimbox_config.run_after ~= nil and type(g.kimbox_config.run_after) == "function" then
-        if not pcall(g.kimbox_config.run_after) then
-            log.err("failed running 'after' function", true)
-        end
-    end
+  end
 end
 
 ---Toggle between kimbox styles
 function M.toggle()
-    local index = g.kimbox_config.toggle_style_index + 1
-    if index > #g.kimbox_config.toggle_style_list then
-        index = 1
-    end
+  local index = g.kimbox_config.toggle_style_index + 1
+  if index > #g.kimbox_config.toggle_style_list then
+    index = 1
+  end
 
-    M.set_options("style", g.kimbox_config.toggle_style_list[index])
-    M.set_options("toggle_style_index", index)
+  M.set_options("style", g.kimbox_config.toggle_style_list[index])
+  M.set_options("toggle_style_index", index)
 
-    vim.o.background = "dark"
-    cmd("colorscheme kimbox")
+  vim.o.background = "dark"
+  cmd("colorscheme kimbox")
 end
 
 ---Setup kimbox.nvim options, without applying colorscheme
 ---@param opts table: a table containing options
 function M.setup(opts)
-    if not g.kimbox_config or not g.kimbox_config.loaded then -- if it's the first time setup() is called
-        g.kimbox_config = vim.tbl_deep_extend("keep", g.kimbox_config or {}, default_config)
+  if not g.kimbox_config or not g.kimbox_config.loaded then -- if it's the first time setup() is called
+    g.kimbox_config = vim.tbl_deep_extend("keep", g.kimbox_config or {}, default_config)
 
-        local old_config = require("kimbox.old_config")
-        if old_config then
-            opts = old_config
-        end
-
-        M.set_options("loaded", true)
-        M.set_options("toggle_style_index", 0)
+    local old_config = require("kimbox.old_config")
+    if old_config then
+      opts = old_config
     end
 
-    if opts then
-        g.kimbox_config = vim.tbl_deep_extend("force", g.kimbox_config, opts)
-        if opts.toggle_style_list then -- this table cannot be extended, it has to be replaced
-            M.set_options("toggle_style_list", opts.toggle_style_list)
-        end
-    end
+    M.set_options("loaded", true)
+    M.set_options("toggle_style_index", 0)
+  end
 
-    vim.keymap.set(
-        "n",
-        g.kimbox_config.toggle_style_key,
-        [[<cmd>lua require('kimbox').toggle()<CR>]],
-        {noremap = true, silent = true}
-    )
+  if opts then
+    g.kimbox_config = vim.tbl_deep_extend("force", g.kimbox_config, opts)
+    if opts.toggle_style_list then -- this table cannot be extended, it has to be replaced
+      M.set_options("toggle_style_list", opts.toggle_style_list)
+    end
+  end
+
+  vim.keymap.set(
+    "n",
+    g.kimbox_config.toggle_style_key,
+    [[<cmd>lua require('kimbox').toggle()<CR>]],
+    { noremap = true, silent = true }
+  )
 end
 
 ---Set the colorscheme
 ---
 ---This is exactly the same as simply doing `colorscheme kimbox`
 function M.load()
-    cmd("colorscheme kimbox")
+  cmd("colorscheme kimbox")
 end
 
 return M
--- lua/kimbox/palette.lua
+++ lua/kimbox/palette.lua
@@ -1,67 +1,90 @@
 local bgs = {
-    medium = "#231A0C",
-    ocean = "#221A02",
-    vscode = "#221A0F",
-    deep = "#0F111B",
-    darker = "#291804"
+  medium = "#262626",
 }
 
 return {
-    colors = {
-        bg1 = "#39260E",
-        bg2 = "#362712",
-        bg3 = "#291804",
-        bg4 = "#5E452B",
-        bg5 = "#5E452B",
-        fg0 = "#D9AE80",
-        fg1 = "#7E602C",
-        fg2 = "#5E452B",
-        fg3 = "#C2A383",
-        fg4 = "#E8C097",
-        ------------------------------------------------------------------------------------------------------
-        black = "#000000",
-        red = "#EF1D55",
-        magenta = "#A06469",
-        orange = "#FF5813",
-        green = "#819C3B",
-        yellow = "#FF9500",
-        aqua = "#7EB2B1",
-        blue = "#4C96A8",
-        purple = "#98676A",
-        --
-        philippine_green = "#088649",
-        sea_green = "#77A172",
-        jade_green = "#2AB074",
-        morning_blue = "#83A598",
-        jelly_bean_blue = "#418292",
-        slate_gray = "#719190",
-        tuscan_red = "#7E5053",
-        maximum_purple = "#733E8B",
-        deep_lilac = "#A25BC4",
-        vista_blue = "#7E9CD8",
-        amethyst = "#938AA9",
-        oni_violet = "#957FB8",
-        salmon = "#EA6962",
-        wave_red = "#E46876",
-        peach_red = "#FF5D62",
-        old_rose = "#BD798B",
-        puce = "#D3869B",
-        pumpkin = "#FE8019",
-        surimi_orange = "#FFA066",
-        dyellow = "#E78A4E", -- "#DB8456"
-        bg_red = "#DC3958",
-        light_red = "#CC6666",
-        -- These are duplicates because they are both used and could be changed at some point
-        coyote_brown = "#7E602C",
-        coyote_brown1 = "#7E602C",
-        grullo_grey = "#A89984",
-        wenge_grey = "#625A5A",
-        operator_base = "#E8C097",
-        ------------------------------------------------------------------------------------------------------
-        diff_add = "#889B4A",
-        diff_delete = "#CC6666",
-        diff_change = "#543739",
-        diff_text = "#325C59" -- "#335250" "#405453"
-    },
-    bgs = bgs
+  colors = {
+    bg1 = "#2f2f2f",
+    bg2 = "#656565",
+    bg3 = "#291804",
+    bg4 = "#5E452B",
+    bg5 = "#5E452B",
+    fg0 = "#D9AE80",
+    fg1 = "#7E602C",
+    fg2 = "#5E452B",
+    fg3 = "#C2A383",
+    fg4 = "#E8C097",
+    ------------------------------------------------------------------------------------------------------
+    black = "#000000",
+    red = "#d75f5f",
+    magenta = "#775759",
+    orange = "#cd950c",
+    green = "#8fee96",
+    yellow = "#d4d232",
+    aqua = "#22c3a1",
+    blue = "#84edb9",
+    purple = "#98676A",
+    --     # black
+    -- color0   #2f2f2f
+    -- color8   #656565
+    --
+    -- # red
+    -- color1   #d75f5f
+    --
+    -- # green
+    -- color2    #d4d232
+    -- color10   #8fee96
+    --
+    -- # yellow
+    -- color3    #af865a
+    -- color11   #cd950c
+    --
+    -- # blue
+    -- color4   #22c3a1
+    --
+    -- # magenta
+    -- color5   #775759
+    --
+    -- # cyan
+    -- color6   #84edb9
+    --
+    -- # white
+    -- color7   #c0b18b
+    -- color15  #d8d8d8
+    --
+    philippine_green = "#088649",
+    sea_green = "#77A172",
+    jade_green = "#2AB074",
+    morning_blue = "#83A598",
+    jelly_bean_blue = "#418292",
+    slate_gray = "#719190",
+    tuscan_red = "#7E5053",
+    maximum_purple = "#733E8B",
+    deep_lilac = "#A25BC4",
+    vista_blue = "#7E9CD8",
+    amethyst = "#938AA9",
+    oni_violet = "#957FB8",
+    salmon = "#EA6962",
+    wave_red = "#E46876",
+    peach_red = "#FF5D62",
+    old_rose = "#BD798B",
+    puce = "#D3869B",
+    pumpkin = "#FE8019",
+    surimi_orange = "#FFA066",
+    dyellow = "#E78A4E", -- "#DB8456"
+    bg_red = "#DC3958",
+    light_red = "#CC6666",
+    -- These are duplicates because they are both used and could be changed at some point
+    coyote_brown = "#7E602C",
+    coyote_brown1 = "#7E602C",
+    grullo_grey = "#A89984",
+    wenge_grey = "#625A5A",
+    operator_base = "#E8C097",
+    ------------------------------------------------------------------------------------------------------
+    diff_add = "#889B4A",
+    diff_delete = "#CC6666",
+    diff_change = "#543739",
+    diff_text = "#325C59" -- "#335250" "#405453"
+  },
+  bgs = bgs
 }
